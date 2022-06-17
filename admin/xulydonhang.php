5b849d504aadfb872a69b4bca8b7490e2f2a4e39
HoangMoon <hoangmoon8321@gmail.com>
2022-06-17 21:32:06 +0700

    phân trang xử lý đơn hàng

--- admin/xulydonhang.php
+++ admin/xulydonhang.php
@@ -30,13 +30,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
     <title>Đơn hàng</title>
 
@@ -48,11 +49,11 @@
     <link href="vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-	
+
     <!-- bootstrap-progressbar -->
     <link href="vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
     <!-- JQVMap -->
-    <link href="vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <link href="vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
     <!-- bootstrap-daterangepicker -->
     <link href="vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
@@ -60,98 +61,115 @@
     <link href="build/css/custom.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/font1.css">
     <link rel="stylesheet" href="./css/page.css">
-  </head>
-  <body class="nav-md">
+
+</head>
+
+<body class="nav-md">
     <div class="container body">
-      <div class="main_container">
-             <?php
+        <div class="main_container">
+            <?php
               include("2404/menu.php");
               include("2404/top.php");
               ?>
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="row">
-          <div class="container">
-    <div class="row">
-       <?php
+            <!-- page content -->
+            <div class="right_col" role="main">
+                <div class="row">
+                    <div class="container">
+                        <div class="row">
+                            <?php
         if(isset($_GET['quanly'])=='xemdonhang') {
           $mahang = $_GET['mahang'];
           $sql_chitiet = mysqli_query($con, "SELECT * FROM tbl_donhang,tbl_sanpham WHERE tbl_donhang.sanpham_id = tbl_sanpham.sanpham_id AND tbl_donhang.mahang = '$mahang' group by donhang_id DESC");
-        ?> 
-        <div class="col-md-12 mb-3">
-          <p style="font-size: 20px;">Xem chi tiết đơn hàng</p>
-                <form action="" method="POST">
-            <table class="table table-bordered">
-            <tr style="text-align:center">
-              <th>STT</th>
-              <th>Mã hàng</th>
-              <th>Tên sản phẩm</th>
-              <th>Số lượng</th>
-              <th>Giá</th>
-              <th>Tổng tiền</th>
-              <th>Ngày tháng đặt</th>
-              <!-- <th>Quản lý</th> -->
-            </tr>
-            <?php
+        ?>
+                            <div class="col-md-12 mb-3">
+                                <p style="font-size: 20px;">Xem chi tiết đơn hàng</p>
+                                <form action="" method="POST">
+                                    <table class="table table-bordered">
+                                        <tr style="text-align:center">
+                                            <th>STT</th>
+                                            <th>Mã hàng</th>
+                                            <th>Tên sản phẩm</th>
+                                            <th>Số lượng</th>
+                                            <th>Giá</th>
+                                            <th>Tổng tiền</th>
+                                            <th>Ngày tháng đặt</th>
+                                            <!-- <th>Quản lý</th> -->
+                                        </tr>
+                                        <?php
             $i= 0;
               while($row_donhang = mysqli_fetch_array($sql_chitiet)){
                 $i++;
             ?>
-              <tr style="text-align:center">
-                <td><?php echo $i ?></td>
-                <td><?php echo $row_donhang['mahang'] ?></td>
-                <td><?php echo $row_donhang['sanpham_name'] ?></td>
-                <td><?php echo $row_donhang['soluong'] ?></td>
-                <td><?php echo number_format($row_donhang['sanpham_giakhuyenmai']).'vnđ' ?></td>
-                <td><?php echo number_format($row_donhang['soluong']*$row_donhang['sanpham_giakhuyenmai']).'vnđ'?></td>
-                <td><?php echo $row_donhang['ngaythang'] ?></td>
-                <input type="hidden" name="mahang_xuly" value="<?php echo $row_donhang['mahang'] ?>">
-              </tr>
-              <?php
+                                        <tr style="text-align:center">
+                                            <td><?php echo $i ?></td>
+                                            <td><?php echo $row_donhang['mahang'] ?></td>
+                                            <td><?php echo $row_donhang['sanpham_name'] ?></td>
+                                            <td><?php echo $row_donhang['soluong'] ?></td>
+                                            <td><?php echo number_format($row_donhang['sanpham_giakhuyenmai']).'vnđ' ?>
+                                            </td>
+                                            <td><?php echo number_format($row_donhang['soluong']*$row_donhang['sanpham_giakhuyenmai']).'vnđ'?>
+                                            </td>
+                                            <td><?php echo $row_donhang['ngaythang'] ?></td>
+                                            <input type="hidden" name="mahang_xuly"
+                                                value="<?php echo $row_donhang['mahang'] ?>">
+                                        </tr>
+                                        <?php
               }
               ?>
-                  </table>
-                  <select name="xuly" id="" class="form-control">
-              <option value="1">Đã xử lý</option>
-              <option value="0">Chưa xử lý</option>
-            </select>
-            <br>
-            <input type="submit" value="Cập nhật đơn hàng" class="btn btn-success" name="capnhatdonhang">
-          </form>
-        </div>
-        <?php
+                                    </table>
+                                    <select name="xuly" id="" class="form-control">
+                                        <option value="1">Đã xử lý</option>
+                                        <option value="0">Chưa xử lý</option>
+                                    </select>
+                                    <br>
+                                    <input type="submit" value="Cập nhật đơn hàng" class="btn btn-success"
+                                        name="capnhatdonhang">
+                                </form>
+                            </div>
+                            <?php
         } else {
-         ?> 
-         
-        <?php
+         ?>
+
+                            <?php
         }
         ?>
-      <div class="col-md-12">
-      <h4>Danh sách đơn hàng</h4>
-      <?php
-        $sql_select = mysqli_query($con, "SELECT *, SUM(tbl_donhang.soluong*tbl_sanpham.sanpham_giakhuyenmai) AS 'tongtien'  FROM tbl_donhang, tbl_sanpham, tbl_khachhang WHERE tbl_donhang.sanpham_id = tbl_sanpham.sanpham_id AND tbl_khachhang.khachhang_id = tbl_donhang.khachhang_id  GROUP BY tbl_donhang.mahang DESC");
+                            <div class="col-md-12">
+                                <h4>Danh sách đơn hàng</h4>
+                                <?php
+                                 if (isset($_GET['trang'])){
+                                   $page = $_GET['trang'];  
+                                 }else{
+                                     $page = '';
+                                 }
+                                 if($page == '' || $page == 1){
+                                     $begin = 0;
+                                 }else{
+                                     $begin = ($page*5)-5;
+                                 }
+                                 
+        $sql_select = mysqli_query($con, "SELECT *, SUM(tbl_donhang.soluong*tbl_sanpham.sanpham_giakhuyenmai) AS 'tongtien'  FROM tbl_donhang, tbl_sanpham, tbl_khachhang WHERE tbl_donhang.sanpham_id = tbl_sanpham.sanpham_id AND tbl_khachhang.khachhang_id = tbl_donhang.khachhang_id  GROUP BY tbl_donhang.mahang DESC LIMIT $begin,5 ");
       ?>
-        <table class="table table-bordered">
-          <tr style="text-align:center">
-            <th>STT</th>
-            <th>Mã hàng</th>
-            <th>Trạng thái đơn hàng</th>
-            <th>Tên khách hàng</th>
-            <th>Ngày tháng đặt</th>
-            <th>Tiền thanh toán</th>
-            <th>Ghi chú</th>
-            <th>Hủy đơn hàng</th>
-            <th>Quản lý</th>
-          </tr>
-          <?php
+                                <table class="table table-bordered">
+                                    <tr style="text-align:center">
+                                        <th>STT</th>
+                                        <th>Mã hàng</th>
+                                        <th>Trạng thái đơn hàng</th>
+                                        <th>Tên khách hàng</th>
+                                        <th>Ngày tháng đặt</th>
+                                        <th>Tiền thanh toán</th>
+                                        <th>Ghi chú</th>
+                                        <th>Hủy đơn hàng</th>
+                                        <th>Quản lý</th>
+                                    </tr>
+                                    <?php
           $i= 0;
             while($row_donhang = mysqli_fetch_array($sql_select)){
               $i++;
           ?>
-            <tr style="text-align:center">
-              <td><?php echo $i ?></td>
-              <td><?php echo $row_donhang['mahang'] ?></td>
-              <td><?php 
+                                    <tr style="text-align:center">
+                                        <td><?php echo $i ?></td>
+                                        <td><?php echo $row_donhang['mahang'] ?></td>
+                                        <td><?php 
                 if($row_donhang['trangthai'] == 0) {
                   echo 'Chưa xử lý';
                 }
@@ -159,12 +177,12 @@
                   echo 'Đã xử lý';
                 }
                ?>
-               </td>
-              <td><?php echo $row_donhang['name'] ?></td>
-              <td><?php echo $row_donhang['ngaythang'] ?></td>
-              <td><?php echo number_format($row_donhang['tongtien']).'vnđ' ?></td>
-              <td><?php echo $row_donhang['note'] ?></td>
-              <td><?php 
+                                        </td>
+                                        <td><?php echo $row_donhang['name'] ?></td>
+                                        <td><?php echo $row_donhang['ngaythang'] ?></td>
+                                        <td><?php echo number_format($row_donhang['tongtien']).'vnđ' ?></td>
+                                        <td><?php echo $row_donhang['note'] ?></td>
+                                        <td><?php 
               if($row_donhang['huydon'] == 0) {
               }
               else if($row_donhang['huydon']==1) {
@@ -174,28 +192,57 @@
                 echo 'Đã hủy đơn';
               }
               ?></td>
-              <td style="text-align: center;"><a style="font-size: 14px;display:block;" href="?xoa=<?php echo $row_donhang['donhang_id'] ?>" class="btn btn-danger mb-2"><i class="fa fa-trash" aria-hidden="true"></i></a> <a href="?quanly=xemdonhang&mahang=<?php echo $row_donhang['mahang'] ?>" style="font-size: 14px;display:block" class="btn btn-primary"><i class="fa fa-eye" aria-hidden="true"></i></a></td>
-            </tr>
-            <?php
+                                        <td style="text-align: center;"><a style="font-size: 14px;display:block;"
+                                                href="?xoa=<?php echo $row_donhang['donhang_id'] ?>"
+                                                class="btn btn-danger mb-2"><i class="fa fa-trash"
+                                                    aria-hidden="true"></i></a> <a
+                                                href="?quanly=xemdonhang&mahang=<?php echo $row_donhang['mahang'] ?>"
+                                                style="font-size: 14px;display:block" class="btn btn-primary"><i
+                                                    class="fa fa-eye" aria-hidden="true"></i></a></td>
+                                    </tr>
+                                    <?php
             }
             ?>
-        </table>
-      </div>
-    </div>
-  </div>
-          </div>
-        </div>
-        <!-- /page content -->
+                                </table>
+                            </div>
+                            <?php
+                            $sql_trang = mysqli_query($con,"SELECT * FROM tbl_donhang");
+                            $row_count = mysqli_num_rows($sql_trang);
+                            $trang = ceil($row_count/7);
+                            ?>
+                            <div class="product-page">
+                                <p>Trang hiện tại: <?php echo $page ?>/<?php echo $trang ?></p>
 
-        <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            @coppyright Bản quyền thuộc by <a href="https://colorlib.com">Huy Hoàng</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
+                                <ul class="list_trang">
+                                    <?php
+                                    for($i=1;$i<=$trang;$i++){
+                                    ?>
+                                    <li
+                                        <?php if($i == $page){echo 'style="background: #EB3349; font-size:14 ;"';}else{ echo '';}?>>
+                                        <a href="xulydonhang.php?trang=<?php echo $i?>"><?php echo $i?></a>
+                                    </li>
+                                    <?php
+                                    }
+                                    ?>
+
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /page content -->
+
+            <!-- footer content -->
+            <footer>
+                <div class="pull-right">
+                    @coppyright Bản quyền thuộc by <a href="https://colorlib.com">Huy Hoàng</a>
+                </div>
+                <div class="clearfix"></div>
+            </footer>
+            <!-- /footer content -->
+        </div>
     </div>
     <!-- jQuery -->
     <script src="vendors/jquery/dist/jquery.min.js"></script>
@@ -237,6 +284,7 @@
 
     <!-- Custom Theme Scripts -->
     <script src="build/js/custom.min.js"></script>
-	
-  </body>
+
+</body>
+
 </html>
